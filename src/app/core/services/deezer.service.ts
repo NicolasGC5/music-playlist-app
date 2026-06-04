14d8ad7e129a516cafb3189b9f767398425512d9
Nicolas Gonzalez <nicolasxd48@gmail.com>
2026-06-04 17:22:28 -0500

    second commit

--- src/app/core/services/deezer.service.ts
+++ src/app/core/services/deezer.service.ts
@@ -8,7 +8,7 @@ import { Song } from '../../shared/models/song.model';
 })
 export class DeezerService {
 
-  private readonly PROXY = 'https://corsproxy.io/?';
+  private readonly PROXY = 'https://api.allorigins.win/get?url=';
   private readonly API = 'https://api.deezer.com';
 
   constructor(private http: HttpClient) { }
@@ -16,15 +16,18 @@ export class DeezerService {
   searchSongs(query: string): Observable<Song[]> {
     const url = `${this.PROXY}${encodeURIComponent(`${this.API}/search?q=${query}`)}`;
     return this.http.get<any>(url).pipe(
-      map(response => response.data.map((item: any) => ({
-        id: item.id,
-        title: item.title,
-        artist: item.artist.name,
-        album: item.album.title,
-        duration: item.duration,
-        preview: item.preview,
-        cover: item.album.cover_medium
-      })))
+      map(response => {
+        const data = JSON.parse(response.contents);
+        return data.data.map((item: any) => ({
+          id: item.id,
+          title: item.title,
+          artist: item.artist.name,
+          album: item.album.title,
+          duration: item.duration,
+          preview: item.preview,
+          cover: item.album.cover_medium
+        }));
+      })
     );
   }
 }
