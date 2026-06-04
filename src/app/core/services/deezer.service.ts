4ca33b961dbf54451abf8f495ae65d5c4cebea47
Nicolas Gonzalez <nicolasxd48@gmail.com>
2026-06-04 17:44:57 -0500

    fixing search

--- src/app/core/services/deezer.service.ts
+++ src/app/core/services/deezer.service.ts
@@ -8,26 +8,23 @@ import { Song } from '../../shared/models/song.model';
 })
 export class DeezerService {
 
-  private readonly PROXY = 'https://api.allorigins.win/get?url=';
+  private readonly PROXY = 'https://thingproxy.freeboard.io/fetch/';
   private readonly API = 'https://api.deezer.com';
 
   constructor(private http: HttpClient) { }
 
   searchSongs(query: string): Observable<Song[]> {
-    const url = `${this.PROXY}${encodeURIComponent(`${this.API}/search?q=${query}`)}`;
+    const url = `${this.PROXY}${this.API}/search?q=${encodeURIComponent(query)}`;
     return this.http.get<any>(url).pipe(
-      map(response => {
-        const data = JSON.parse(response.contents);
-        return data.data.map((item: any) => ({
-          id: item.id,
-          title: item.title,
-          artist: item.artist.name,
-          album: item.album.title,
-          duration: item.duration,
-          preview: item.preview,
-          cover: item.album.cover_medium
-        }));
-      })
+      map(response => response.data.map((item: any) => ({
+        id: item.id,
+        title: item.title,
+        artist: item.artist.name,
+        album: item.album.title,
+        duration: item.duration,
+        preview: item.preview,
+        cover: item.album.cover_medium
+      })))
     );
   }
 }
