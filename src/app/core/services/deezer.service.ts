0f5f859bd8034f72c103faa4357f1d3c4a483406
Nicolas Gonzalez <nicolasxd48@gmail.com>
2026-06-05 12:15:17 -0500

    improve vercel proxy 2

--- src/app/core/services/deezer.service.ts
+++ src/app/core/services/deezer.service.ts
@@ -10,11 +10,15 @@ export class DeezerService {
 
   private readonly PROXY = 'https://corsproxy.io/?';
   private readonly API = 'https://api.deezer.com';
+  private readonly isLocalhost = window.location.hostname === 'localhost';
 
   constructor(private http: HttpClient) { }
 
   searchSongs(query: string): Observable<Song[]> {
-    const url = `${this.PROXY}${encodeURIComponent(`${this.API}/search?q=${query}`)}`;
+    const url = this.isLocalhost
+      ? `${this.PROXY}${encodeURIComponent(`${this.API}/search?q=${query}`)}`
+      : `/api/deezer?q=${encodeURIComponent(query)}`;
+
     return this.http.get<any>(url).pipe(
       map(response => response.data.map((item: any) => ({
         id: item.id,
